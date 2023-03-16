25ed2381bf247ae24ecbe2a125e1fb8a5f0b3e61
Ivan <123951392+1vvan@users.noreply.github.com>
2023-03-16 10:28:38 +0200

    Adding link

--- src/config/config-twitter.js
+++ src/config/config-twitter.js
@@ -1,7 +1,7 @@
 const twitterConfig = {
   twitter: {
     shareUrl: 'https://twitter.com/intent/tweet',
-    shareText: 'Check out this Meetup with SoCal!',
+    shareText: 'Check out this Meetup with SoCal! https://conference-registration-steel.vercel.app/',
   },
 };
 
